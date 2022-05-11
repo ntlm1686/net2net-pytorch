f26050eba54f35c887db2cf46ba6afb81504dad9
Jiajie Li <ljjsalt@gmail.com>
2022-05-11 01:45:03 +0000

    Update not inplace functions

--- net2net/net2net.py
+++ net2net/net2net.py
@@ -26,6 +26,11 @@ def wider_ln_(ln_teacher: nn.Module, ln_student: nn.Module, g: List[int]):
         ln_student.weight.data[j] = ln_teacher.weight.data[g[j]]
         ln_student.bias.data[j] = ln_teacher.bias.data[g[j]]
 
+def rmap(
+    n: int,
+    q: int,
+):
+    return random_mapping_vector(range(q), n)
 
 def map_g(
     l1_teacher: nn.Module,
@@ -75,5 +80,56 @@ def wider_(
                 l2_student.bias.data = l2_teacher.bias.data
 
 
-def deeper():
-    pass
+def wider_first_(
+    l1_teacher: nn.Module,
+    l1_student: nn.Module,
+    g: List[int]
+) -> None:
+    student_hidden = l1_student.weight.shape[0]
+    for j in range(student_hidden):
+        l1_student.weight.data[j] = l1_teacher.weight.data[g[j]]
+    if l1_student.bias is not None:
+        if l1_teacher.bias is None:
+            l1_student.bias.data.fill_(0)
+        else:
+            for j in range(student_hidden):
+                l1_student.bias.data[j] = l1_teacher.bias.data[g[j]]
+
+
+def wider_second_(
+    l2_teacher: nn.Module,
+    l2_student: nn.Module,
+    g: List[int]
+) -> None:
+    student_hidden = l2_student.weight.shape[1]
+    teacher_out = l2_teacher.weight.shape[0]
+    for j in range(student_hidden):
+        l2_student.weight.data[:teacher_out, j] = 1/g.count(g[j]) \
+            * l2_teacher.weight.data[:, g[j]]
+    if l2_student.bias is not None:
+        if l2_teacher.bias is None:
+            l2_student.bias.data.fill_(0)
+        else:
+            for j in range(student_hidden):
+                l2_student.bias.data[:teacher_out] = l2_teacher.bias.data
+
+
+def wider_first(
+    l1_teacher: nn.Module,
+    hidden_size: int,
+    g: List[int],
+    # dim: int = 1 # dim to expand
+):
+    weight = torch.stack([l1_teacher.weight.data[g[j]] for j in range(hidden_size)], dim=0)
+    bias = torch.stack([l1_teacher.bias.data[g[j]] for j in range(hidden_size)], dim=0)
+    return weight, bias
+
+
+def wider_second(
+    l2_teacher: nn.Module,
+    hidden_size: int,
+    g: List[int]
+):
+    weight = torch.stack([1/g.count(g[j]) * l2_teacher.weight.data[:, g[j]] for j in range(hidden_size)], dim=1)
+    bias = l2_teacher.bias.data
+    return weight, bias
